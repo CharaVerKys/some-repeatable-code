2bc780543a8e1bc4a631a5eee3c21a489cb595e8
CharaVerKys <charaverk@gmail.com>
2025-04-25 21:49:58 +0300

    gcc fixes

--- somemacroses.hpp
+++ somemacroses.hpp
@@ -5,8 +5,3 @@ static_assert([]() constexpr->bool{expr; return true;}())
 
 #define isConstexpr_no_discard(expr) \
 static_assert([]() constexpr->bool{auto f = expr; return true;}())
-
-#define PRIVATE_CONTEXT(seq) \
-    private: \
-    seq \
-    public:
