0f26e1b1312d7412995f84634dc652d4996705b8
CharaVerKys <163038037+CharaVerKys@users.noreply.github.com>
2026-03-13 12:51:17 +0300

    fix static
    (it was called static, for weird reasons, this whole time and i only noticed when tested explicitly)

--- threadchecking.hpp
+++ threadchecking.hpp
@@ -1,11 +1,11 @@
 #pragma once
-#include <cassert>
+#include "cussert.hpp"
 #include <thread>
 #include <cstdarg>
 #include <vector>
 
-static inline std::thread::id mainThreadID;
-static inline std::thread::id notMainThreadID;
+inline std::thread::id mainThreadID;
+inline std::thread::id notMainThreadID;
 
 // unlike qt here usings objects, not void*
 // but uses like '&threadID' because va_list not supports non-pod args (thread::id is not)
@@ -14,46 +14,46 @@ static inline std::thread::id notMainThreadID;
 
 inline void checkAllThreadIDsCollision() {
     std::vector<std::thread::id> threadIDs = {
-        mainThreadID,
-        notMainThreadID
+     mainThreadID,
+     notMainThreadID
     };
 
     for (size_t i = 0; i < threadIDs.size(); ++i) {
         for (size_t j = i + 1; j < threadIDs.size(); ++j) {
             if (threadIDs[j] not_eq std::thread::id()) {
-                assert(threadIDs[i] != threadIDs[j] && "Thread ID collision");
+                cussert(threadIDs[i] != threadIDs[j] && "Thread ID collision");
             }
         }
     }
 }
 
-#define NOTmainThread assert(std::this_thread::get_id() not_eq mainThreadID);
+#define NOTmainThread cussert(std::this_thread::get_id() not_eq mainThreadID);
 
 inline void checkThread(std::thread::id* thread)
 {
     if (*thread == std::thread::id()) {
         *thread = std::this_thread::get_id();
     } else {
-        assert(*thread == std::this_thread::get_id());
+        cussert(*thread == std::this_thread::get_id());
     }
     checkAllThreadIDsCollision();
 }
 
 inline void checkThreadIsNOT(const std::thread::id* thread)
 {
-    assert(*thread not_eq std::this_thread::get_id()); 
+    cussert(*thread not_eq std::this_thread::get_id()); 
 }
 
 inline void checkThread(int count...)
 {
     va_list args;
-    assert(count>=1);
+    cussert(count>=1);
     uint8_t allCount = (uint8_t)count;
     va_start(args, count); // warning by clang: UB if count have type uint8_t or uint16_t (1/2 bytes), Ok if uint // idk why
     bool was_eq = false;
     while(count){
         const std::thread::id *const id = va_arg(args, std::thread::id*);
-        if(allCount == 1 and std::this_thread::get_id() not_eq *id){assert(false);}
+        if(allCount == 1 and std::this_thread::get_id() not_eq *id){cussert(false);}
         if(not id){
             va_end(args);
             return;
@@ -64,8 +64,8 @@ inline void checkThread(int count...)
         --count;
     }//while
     va_end(args);
-    assert(count == 0);
-    assert(was_eq);
+    cussert(count == 0);
+    cussert(was_eq);
 }
 
 #else
